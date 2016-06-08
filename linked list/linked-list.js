8b458f40a73d06879dc19227c8f0fe0ca536a068
Anthony Rogers <amrogers75@gmail.com>
2016-06-08 07:48:50 -0600

    cleand up code by  adding them to directories

---
+++ linked list/linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Created by anthonyrogers on 6/7/16.
+ */
+function LinkedList() {
+    this.head = null;
+    
+    this.insert = function (element) {
+        
+    };
+
+    this.remove = function (element) {
+
+    };
+
+    this.contains = function (element) {
+
+    };
+
+    this.size = function () {
+
+    };
+}
+
+function Node(value) {
+    this.value = value;
+    this.next=null;
+}
+
+/**var newnode = newnode(value);
+
+function addtolist(node, value) {
+    if(node.next == null)
+        node.next= newnode(value);
+    eles;
+    addtolist(node.next, value)
+
+};*/
