7666c17ae0911a864c864fda849850730dcb0d4e
Anthony Rogers <amrogers75@gmail.com>
2016-06-16 07:43:21 -0600

    added tree type data info

--- dictionary-datastuff/dictionary.js
+++ dictionary-datastuff/dictionary.js
@@ -36,3 +36,73 @@ mypbook.add("Fred", "444-4444");
 console.log("mikes number is:" + mypbook.find("Mike"));
 mypbook.remove("Mike");
 mypbook.showAll();
+
+
+// tue. 06/14/16 data structure
+
+// constructor 1
+function Node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = show;
+}
+
+//constructor 2
+function BST() {
+    this.root = null;
+    this.insert = insert;
+    this.inOrder = inOrder;
+}
+
+function show(){
+    return this.data;
+}
+
+function insert(data){
+    var n = new Node(data, null, null);
+    if(this.root === null){
+        this.root = n;
+    }
+    else{
+        var current = this.root;
+        var parent;
+        while(true){
+            parent = current;
+            if(data < current.data){
+                current = current.left;
+                if(current === null){
+                    parent.left = n;
+                    break;
+                }
+            }
+            else{
+                current = current.right;
+                if(current === null){
+                    parent.right = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function inOrder(node) {
+    if(node !== null){
+        inOrder(node.left);
+        console.log(node.show() + "");
+        inOrder(node.right);
+    }
+}
+
+// BST is binary search tree
+var num = BST();
+num.insert(45);
+num.insert(36);
+num.insert(2);
+num.insert(88);
+num.insert(24);
+num.insert(18);
+
+console.log("inOrder traversal:");
+num.inOrder(num.root);
